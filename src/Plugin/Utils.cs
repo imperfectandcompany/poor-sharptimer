e88e4535f494c7f8e6eaf7ef8743f32511d6ab57
raz <9876747+razpbrry@users.noreply.github.com>
2024-06-14 11:17:22 -0500

    Change point calc base points to 1000

--- src/Plugin/Utils.cs
+++ src/Plugin/Utils.cs
@@ -224,7 +224,7 @@ namespace SharpTimer
 
         public double CalculatePoints(int timerTicks, int style)
         {
-            double basePoints = 10000.0;
+            double basePoints = 1000.0;
             double timeFactor = 0.0001;
             double tierMult = 0.1;
             double styleMult = GetStyleMultiplier(style);
